e302df316be95a0b955fe8ff7cc5286ab7e50402
Greg Hepworth <greg.hepworth@me.com>
2020-04-20 16:43:42 +0100

    Updated use of HMV to handle the new error handling

--- Jewel/Models/DataSource.swift
+++ Jewel/Models/DataSource.swift
@@ -12,16 +12,27 @@ var releasesData = load()
 let store = HMV()
 
 func load() -> [Release] {
+    
+    var releaseUrls = [String]()
+    
+    releaseUrls.append("https://music.apple.com/gb/album/all-that-must-be/1322664114")
+    releaseUrls.append("https://music.apple.com/gb/album/based-on-a-true-story/1241281467")
+    releaseUrls.append("https://music.apple.com/gb/album/the-fat-of-the-land/1450123945")
+    releaseUrls.append("https://music.apple.com/gb/album/journey-inwards/595779873")
+    releaseUrls.append("https://music.apple.com/gb/album/exit-planet-dust/723670972")
+    releaseUrls.append("https://music.apple.com/gb/album/ok-computer/1097861387")
+    releaseUrls.append("https://music.apple.com/gb/album/psyence-fiction/1440922148")
+    releaseUrls.append("https://music.apple.com/gb/album/sincere-deluxe/1440230518")
+    
     var releasesData = [Release]()
     
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/all-that-must-be/1322664114"))
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/based-on-a-true-story/1241281467"))
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/the-fat-of-the-land/1450123945"))
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/journey-inwards/595779873"))
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/exit-planet-dust/723670972"))
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/ok-computer/1097861387"))
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/psyence-fiction/1440922148"))
-    releasesData.append(store.getRelease(appleMusicShareURL: "https://music.apple.com/gb/album/sincere-deluxe/1440230518"))
+    for url in releaseUrls {
+        do {
+            releasesData.append(try store.getReleaseWith(shareUrl: url))
+        } catch {
+            print("There was an error loading the fake data: \(error).")
+        }
+    }
     
     return releasesData
 }
