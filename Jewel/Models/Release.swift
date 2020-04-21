d176abbdc033fef6169a0271afe2e06ba5344b5f
Greg Hepworth <greg.hepworth@me.com>
2020-04-21 17:03:01 +0100

    Updated to use Cider - an Apple Music API wrapper.

---
+++ Jewel/Models/Release.swift
@@ -0,0 +1,19 @@
+//
+//  Release.swift
+//  Jewel
+//
+//  Created by Greg Hepworth on 21/04/2020.
+//  Copyright © 2020 Breakbeat Limited. All rights reserved.
+//
+
+import Foundation
+
+public struct Release: Identifiable {
+    
+    public var id: String
+    public var appleMusicShareURL: URL
+    public var title: String
+    public var artist: String
+    public var artworkURL: String
+    
+}
--- Jewel/Models/Wallet.swift
+++ Jewel/Models/Wallet.swift
@@ -7,31 +7,43 @@
 //
 
 import Foundation
-import HMV
+import Cider
 
 class Wallet: ObservableObject {
     @Published var releases = [Release]()
     
     init() {
-        var releaseUrls = [String]()
         
-        releaseUrls.append("https://music.apple.com/gb/album/all-that-must-be/1322664114")
-        releaseUrls.append("https://music.apple.com/gb/album/based-on-a-true-story/1241281467")
-        releaseUrls.append("https://music.apple.com/gb/album/the-fat-of-the-land/1450123945")
-        releaseUrls.append("https://music.apple.com/gb/album/journey-inwards/595779873")
-        releaseUrls.append("https://music.apple.com/gb/album/exit-planet-dust/723670972")
-        releaseUrls.append("https://music.apple.com/gb/album/ok-computer/1097861387")
-        releaseUrls.append("https://music.apple.com/gb/album/psyence-fiction/1440922148")
-        releaseUrls.append("https://music.apple.com/gb/album/sincere-deluxe/1440230518")
-        
-        let store = HMV()
-        
-        for url in releaseUrls {
-            do {
-                releases.append(try store.getReleaseWith(shareUrl: url))
-            } catch {
-                print("There was an error loading the fake data: \(error).")
-            }
+        if let developerToken = Bundle.main.infoDictionary?["APPLE_MUSIC_API_TOKEN"] as? String {
+            loadExampleWallet(developerToken)
+        } else {
+            print("No Apple Music API Token Found!")
         }
     }
+    
+    fileprivate func loadExampleWallet(_ developerToken: String) {
+        let cider = CiderClient(storefront: .unitedKingdom, developerToken: developerToken)
+        
+        let releases = ["1322664114", "1241281467", "1450123945", "595779873", "723670972", "1097861387", "1440922148", "1440230518"]
+        
+        for release in releases {
+            cider.album(id: release, completion: {
+                (album: Album?, error: Error?) -> Void in
+                DispatchQueue.main.async {
+                    let attributes = album?.attributes
+                    
+                    let artworkUrl = attributes?.artwork.url.replacingOccurrences(of: "{w}", with: "1000").replacingOccurrences(of: "{h}", with: "1000")
+                    
+                    let release = Release(id: album!.id, appleMusicShareURL: attributes!.url, title: attributes!.name, artist: attributes!.artistName, artworkURL: artworkUrl ?? "")
+                    
+                    self.add(release: release)
+                }
+            })
+        }
+    }
+    
+    fileprivate func add(release: Release) {
+        releases.append(release)
+    }
+    
 }
