787d12f2b18b61c925cba64ef32ddec33e22c097
Greg Hepworth <greg@breakbeat.io>
2020-04-28 20:03:50 +0100

    If the docs are right then this should sync wallet over iCloud
    (hard to test until its built)

--- Jewel/SceneDelegate.swift
+++ Jewel/SceneDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import SwiftUI
+import Zephyr
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -57,6 +58,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        Zephyr.sync(keys: "savedWallet")
     }
 
 
