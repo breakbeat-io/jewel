8f1efc80c96a04a6b96f10cf3b599398391af9bb
Greg Hepworth <greg.hepworth@me.com>
2020-05-05 09:06:09 +0100

    Minor language changes

--- Jewel/Views/Options/Options.swift
+++ Jewel/Views/Options/Options.swift
@@ -36,15 +36,15 @@ struct Options: View {
                             )
                         }
                     }
-                    Section(footer: Text("Load a selection of current and classic releases, chosen by the music lovers that make Jewel.")) {
+                    Section(footer: Text("Load a selection of recent and classic releases, chosen by the music lovers that make Jewel.")) {
                         Button(action: {
                             self.showLoadRecommendationsAlert = true
                         }) {
                             Text("Load Recommendations")
                         }
                         .alert(isPresented: $showLoadRecommendationsAlert) {
-                            Alert(title: Text("Do you want to load our recommendations?"),
-                                  message: Text("This will remove your current selections."),
+                            Alert(title: Text("Are you sure you want to load our recommendations?"),
+                                  message: Text("This will remove your current collection."),
                                   primaryButton: .cancel(Text("Cancel")),
                                   secondaryButton: .default(Text("Load").bold()) {
                                     self.userData.loadRecommendations()
@@ -59,7 +59,7 @@ struct Options: View {
                     }
                     .foregroundColor(.red)
                     .alert(isPresented: $showDeleteAllWarning) {
-                        Alert(title: Text("Are you sure you want to delete all albums in your wallet?"),
+                        Alert(title: Text("Are you sure you want to delete all albums in your collection?"),
                               message: Text("You cannot undo this operation."),
                               primaryButton: .cancel(Text("Cancel")),
                               secondaryButton: .destructive(Text("Delete")) {
