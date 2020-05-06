856ef243bb8663b39007884faaadeafe69f087ff
Greg Hepworth <greg.hepworth@me.com>
2020-05-06 16:06:04 +0100

    Added a Home view to load as default for double columns views (iPad / macOS)

---
+++ Jewel/Views/Start.swift
@@ -0,0 +1,21 @@
+//
+//  Start.swift
+//  Jewel
+//
+//  Created by Greg Hepworth on 06/05/2020.
+//  Copyright © 2020 Breakbeat Limited. All rights reserved.
+//
+
+import SwiftUI
+
+struct Start: View {
+    var body: some View {
+        Text("Jewel")
+    }
+}
+
+struct Start_Previews: PreviewProvider {
+    static var previews: some View {
+        Start()
+    }
+}
