fbe5fa7b30ff41ee5bd98c44fe02fd4ab9118f7d
Greg Hepworth <greg.hepworth@me.com>
2020-05-21 00:11:23 +0100

    Lovely tuple use on playback links!

--- Jewel/Views/Components/PrimaryPlaybackLink.swift
+++ Jewel/Views/Components/PrimaryPlaybackLink.swift
@@ -13,43 +13,36 @@ struct PrimaryPlaybackLink: View {
     @EnvironmentObject var userData: UserData
     var slotIndex: Int
     
-    var body: some View {
-        
-        var playbackLink: URL?
-        var playbackName: String
-        
+    var playbackDetails: (name: String, url: URL) {
         let preferredProvider = OdesliPlatform.allCases[userData.preferences.preferredMusicPlatform]
         
         if let providerLink = userData.activeCollection.slots[slotIndex].playbackLinks?.linksByPlatform[preferredProvider.rawValue] {
-            playbackLink = providerLink.url
-            playbackName = preferredProvider.friendlyName
+            return (preferredProvider.friendlyName, providerLink.url)
         } else {
-            playbackLink = userData.activeCollection.slots[slotIndex].source?.content?.attributes?.url
-            playbackName = OdesliPlatform.appleMusic.friendlyName
+            return (OdesliPlatform.appleMusic.friendlyName, userData.activeCollection.slots[slotIndex].source!.content!.attributes!.url)
         }
-        
-        let playbackLinkView =
-            IfLet(playbackLink) { url in
-                Button(action: {
-                    UIApplication.shared.open(url)
-                }) {
-                    HStack {
-                        Image(systemName: "play.fill")
-                            .font(.headline)
-                        Text("Play in \(playbackName)")
-                            .font(.headline)
-                    }
-                    .padding()
-                    .foregroundColor(.primary)
-                    .cornerRadius(40)
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 40)
-                            .stroke(Color.primary, lineWidth: 2)
-                    )
+    }
+    
+    var body: some View {
+        Group {
+            Button(action: {
+                UIApplication.shared.open(self.playbackDetails.url)
+            }) {
+                HStack {
+                    Image(systemName: "play.fill")
+                        .font(.headline)
+                    Text("Play in \(playbackDetails.name)")
+                        .font(.headline)
                 }
+                .padding()
+                .foregroundColor(.primary)
+                .cornerRadius(40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 40)
+                        .stroke(Color.primary, lineWidth: 2)
+                )
+            }
         }
-        
-        return playbackLinkView
     }
 }
 
