3c3a37fdfe6e7ec49a51b5dd135085f177535ad8
Greg Hepworth <greg.hepworth@me.com>
2020-05-24 22:21:46 +0100

    Now follows the system colours for dark mode support

--- Jewel/Views/Welcome.swift
+++ Jewel/Views/Welcome.swift
@@ -19,7 +19,7 @@ struct Welcome: View {
                     .opacity(0.1) // hack to prevent clickthrough at opactity 0
                     .edgesIgnoringSafeArea(.all)
                 Rectangle()
-                    .fill(Color.white)
+                    .fill(Color(UIColor.systemBackground))
                     .frame(width: geo.size.width * 0.8, height: geo.size.height * 0.9)
                     .cornerRadius(20)
                     .shadow(radius: 5)
@@ -44,12 +44,12 @@ struct Welcome: View {
                             }
                             .padding(.horizontal)
                             Rectangle()
-                                .fill(LinearGradient(gradient: Gradient(colors: [.white, Color.white.opacity(0)]), startPoint: .top, endPoint: .bottom))
-                                .frame(height: 25)
+                                .fill(LinearGradient(gradient: Gradient(colors: [Color(UIColor.systemBackground), Color(UIColor.systemBackground).opacity(0)]), startPoint: .top, endPoint: .bottom))
+                                .frame(height: 10)
                             Rectangle()
-                                .fill(LinearGradient(gradient: Gradient(colors: [.white, Color.white.opacity(0)]), startPoint: .bottom, endPoint: .top))
-                                .frame(height: 25)
-                                .offset(y: geo.size.height - 25)
+                                .fill(LinearGradient(gradient: Gradient(colors: [Color(UIColor.systemBackground).opacity(0), Color(UIColor.systemBackground)]), startPoint: .top, endPoint: .bottom))
+                                .frame(height: 10)
+                                .offset(y: geo.size.height - 10)
                         }
                         
                     }
