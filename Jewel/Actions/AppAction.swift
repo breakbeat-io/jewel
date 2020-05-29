232d21ba3cc04cd7e5efd38f27e377b03b51eca6
Greg Hepworth <greg.hepworth@me.com>
2020-05-29 15:19:22 +0100

    Wired up the secrets file

--- Jewel/Actions/AppAction.swift
+++ Jewel/Actions/AppAction.swift
@@ -36,11 +36,14 @@ func updateSearch(state: SearchState, action: AppAction) -> SearchState {
     case .search(let term):
         Store.appleMusic.search(term: term, limit: 20, types: [.albums]) { storeResults, error in
             DispatchQueue.main.async {
+                
                 print("there were some search results from the API")
 //                self.results = storeResults?.albums?.data
             }
         }
         print("search term populated wtih \(term)")
+    case .populateSearchResults(let results):
+        state.results = results
     case .removeSearchResults:
         state.results = nil
     default: break
--- Jewel/Actions/AppActions.swift
+++ Jewel/Actions/AppActions.swift
@@ -13,5 +13,6 @@ enum AppAction {
     case addAlbum(album: Album)
     case removeAlbum(at: IndexSet)
     case search(term: String)
+    case populateSearchResults(results: [Album])
     case removeSearchResults
 }
