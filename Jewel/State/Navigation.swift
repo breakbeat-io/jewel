dbf8a5efc72a4c4093ee3028db628b4419a6a270
Greg Hepworth <greg@breakbeat.io>
2020-07-04 19:04:59 +0100

    Refactored card height into one func as was the same for both

--- Jewel/State/Navigation.swift
+++ Jewel/State/Navigation.swift
@@ -55,13 +55,16 @@ struct Navigation {
   
   var libraryViewHeight: CGFloat = 812
   var collectionCardHeight: CGFloat {
-    let height = (self.libraryViewHeight - 200) / 8
-    return height < 61 ? 61 : height
+    cardHeightFor(libraryViewHeight)
   }
   
   var collectionViewHeight: CGFloat = 812
   var albumCardHeight: CGFloat {
-    let height = (self.collectionViewHeight - 200) / 8
+    cardHeightFor(collectionViewHeight)
+  }
+  
+  private func cardHeightFor(_ viewHeight: CGFloat) -> CGFloat {
+    let height = (viewHeight - 200) / 8
     return height < 61 ? 61 : height
   }
   
