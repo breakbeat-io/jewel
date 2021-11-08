ba04179175936429ceece4526b8f235aae909593
Greg Hepworth <greg@breakbeat.io>
2021-11-08 21:28:46 +0000

    Configures Xcode Cloud to write to secrets post clone.

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Jewel
+#
+#  Created by Greg Hepworth on 08/11/2021.
+#  Copyright © 2021 Breakbeat Ltd. All rights reserved.
+
+set -euo pipefail
+
+: "${FIREBASE_API_KEY:?Firebase API Key environment variable not set or empty}"
+
+cat > $CI_PROJECT_FILE_PATH/Jewel/App/Secrets.swift << EOF
+struct Secrets {
+  static var firebaseAPIKey = $FIREBASE_API_KEY
+}
+EOF
